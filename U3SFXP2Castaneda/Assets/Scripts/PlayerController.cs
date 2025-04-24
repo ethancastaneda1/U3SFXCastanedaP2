e9ad279b983856fc0345703272f7aae40b50f3a5
ethancastaneda1 <726750@student.dusd.net>
2025-04-24 09:59:03 -0700

    3.3 work on the jumps
    Work on SFX U3 on the 3rd section of the coding project. figure out why jumping animation dosent work

--- U3SFXP2Castaneda/Assets/Scripts/PlayerController.cs
+++ U3SFXP2Castaneda/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float gravityModifier;
     public float jumpForce;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,12 +24,23 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
         {           
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false; 
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
             
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+        
+      
     }
 }
--- U3SFXP2Castaneda/Assets/Scripts/SpawnManager.cs
+++ U3SFXP2Castaneda/Assets/Scripts/SpawnManager.cs
@@ -8,10 +8,12 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2;
     private float repeatRate = 2;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,11 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+       
     }
 }
 
